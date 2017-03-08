fea549bd69e2fc1e99f7ffbaba9dad287c57aac2
xieguanglei <xieguanglei@hotmail.com>
2017-03-08 20:21:13 +0800

    add day5

--- pages/day5-character/index.js
+++ pages/day5-character/index.js
@@ -5,31 +5,53 @@
   var WIDTH = 500;
   var HEIGHT = 500;
 
+  // var points = getPointList('X');
 
-  var points = getPointList();
-  console.log(points);
 
-  for(var i = 1; i < points.length; i++) {
-    var p = points[i];
-    draw(p.x * WIDTH, p.y * HEIGHT, 5, '#999', 1);
+  var words = '你好，世界'.split('');
+  var wordsPoints = words.map(getPointList);
+
+  var start = Date.now();
+
+  loop();
+
+  function loop() {
+
+    var duration = 2000;
+
+    var lasted = Date.now() - start;
+
+    var ii = Math.floor(lasted % (duration * words.length) / duration);
+    var points = wordsPoints[ii];
+    var p = (lasted % duration) / duration;
+
+    var opacity = p < 0.5 ? Math.min(1, 5 * p) : Math.min(1, Math.abs(5 - 5 * p));
+
+    c.clearRect(0, 0, WIDTH, HEIGHT);
+
+    for(var i = 1; i < points.length; i++) {
+      var p = points[i];
+      p.phase += 0.01;
+      var phase = Math.abs(Math.sin(p.phase));
+      draw(p.x * WIDTH, p.y * HEIGHT, p.size * phase, p.color, opacity);
+    }
+    requestAnimationFrame(loop);
   }
 
-
-  function getPointList() {
+  function getPointList(char) {
 
     var c = document.createElement('canvas');
-    document.body.appendChild(c);
     c.width = 50;
     c.height = 50;
     var ctx = c.getContext('2d');
 
     ctx.font = '50px serif';
     ctx.fillStyle = '#999';
-    ctx.fillText('E', 8, 45);
+    ctx.fillText(char, 0, 40);
 
     var imageData = ctx.getImageData(0, 0, 50, 50);
 
-    var count = 10000;
+    var count = 5000;
     var points = [];
     for(var i = 0; i < count; i++) {
       var x = Math.random();
@@ -42,7 +64,10 @@
       if (imageData.data[a]) {
         points.push({
           x: x,
-          y: y
+          y: y,
+          phase: Math.random() * Math.PI * 2,
+          color: randomColor(),
+          size: Math.random() * 10 + 4
         })
       }
     }
@@ -58,5 +83,9 @@
     c.fill();
   }
 
+  function randomColor() {
+    const values = _.values(FLATUI_COLORS);
+    return values[Math.floor(Math.random() * values.length)]
+  }
 
 })();
